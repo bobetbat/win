26454a94483326a36dd95f0943bf9bb41e52aa4e
bobetbat <b.bobetayev.97@gmail.com>
2022-05-26 11:10:46 +0200

    wip

--- packages/lpms-server/build.sh
+++ packages/lpms-server/build.sh
@@ -5,6 +5,10 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+docker -v
+
 docker build --platform=linux/x86_64 \
       --progress=plain \
       -t windingtree/lpms-server:$LPMS_SERVER_TAG \
